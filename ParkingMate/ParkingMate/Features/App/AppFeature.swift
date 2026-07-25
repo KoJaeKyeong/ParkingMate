9faa42a7573c6e7f8363de44062396f39c7dff79
KoJaeKyeong <folw159@gmail.com>
2026-07-25 23:03:18 +0900

    chore: 테스트 타깃 배포 타깃을 앱과 동일한 iOS 17.0으로 정렬
    테스트 타깃만 26.1로 잡혀 있어 하위 버전 시뮬레이터에서 테스트가 실행되지 않았다.

---
+++ ParkingMate/ParkingMate/Features/App/AppFeature.swift
@@ -0,0 +1,8 @@
+//
+//  AppFeature.swift
+//  ParkingMate
+//
+//  Created by 고재경 on 7/25/26.
+//
+
+import Foundation
---
+++ ParkingMate/ParkingMate/Features/App/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  ParkingMate
+//
+//  Created by 고재경 on 7/25/26.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
